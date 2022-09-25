75bb2231b674a41e02ace78b22cc9e92083a360c
Necralma <lihuel.almada@gmail.com>
2022-09-25 11:59:40 -0300

    init and comment estructure

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <SocialIcon
+          url="https://www.twitch.tv/necralma"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+      <div>asd</div>
+    </header>
+  );
+}
+
+export default Header;
