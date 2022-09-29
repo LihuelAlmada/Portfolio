12ea158ec46a33dcc3b7f98ae87ffa3e3faef152
Necralma <lihuel.almada@gmail.com>
2022-09-29 08:20:09 -0300

    finish circle animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,8 +14,12 @@ function Hero() {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://lh3.googleusercontent.com/GtRYBjGDFoHrrFj83I3zcSc7JEW9e5p3heKb8z8zCgPDkb6SkeGjZeYw9szKfLHIrgPUNCgkpTSekJApcwJR9bN_d-BpX7iPM57gHmlGpvI6yFSvtHSYp8sDmIq53s0mqQ=w1280"
+        alt=""
+        />
       <h1>
-
       <span>{text}</span>
       <Cursor cursorColor="#F7AB0A" />
       </h1>
