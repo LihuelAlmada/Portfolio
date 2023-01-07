40bbfc9b71f613ff679565a7c598220ce435800a
Necralma <53120267+LihuelAlmada@users.noreply.github.com>
2023-01-07 08:16:58 -0300

    improve scrollbar

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -13,7 +13,7 @@ type Inputs = {
 export default function ContactMe({}: Props) {
 
   const { register, handleSubmit } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = formData => console.log(formData);
+  const onSubmit: SubmitHandler<Inputs> = formData => {window.location.href = `mailto:lihuel.almada@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name} (${formData.email}). ${formData.message}`};
 
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,11 @@ import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
 import ContactMe from '../components/ContactMe'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Lihuel's Portfolio</title>
       </Head>
@@ -34,6 +35,17 @@ const Home: NextPage = () => {
         <ContactMe />
       </section>
 
+      <Link href="#hero">
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className='flex items-center justify-center'>
+            <img 
+              className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0'
+              src='https://media-exp1.licdn.com/dms/image/C4D03AQF26LYC6KtHYA/profile-displayphoto-shrink_800_800/0/1636391899518?e=1675900800&v=beta&t=vHv54Ktdj7jNJnuSTlEhZDsew9gxyzgeSfM77nHbpb4'
+              alt=''
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   )
 }
