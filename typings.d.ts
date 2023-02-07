baf0c1f3dab52182c5b20574981684c3c8a52940
Necralma <53120267+LihuelAlmada@users.noreply.github.com>
2023-02-07 10:38:55 -0300

    add all typings and gets api necesary for the project

---
+++ typings.d.ts
@@ -0,0 +1,66 @@
+import Skill from './components/Skill';
+import Projects from './components/Projects';
+interface SanityBody {
+  _createdAt: string,
+  _id:        string,
+  _rev:       string,
+  _updatedAt: string,
+}
+
+interface Image{
+  _type:    "image"
+  asset:{
+    _ref:   string
+    _type:  "reference"
+  } 
+}
+
+export interface PageInfo extends SanityBody {
+  _type:                  "pageInfo",
+  backgroundInformation:  string,
+  email:                  string,
+  role:                   string,
+  heroImage:              Image,
+  name:                   string,
+  profilePic:             Image,
+  //socials
+}
+
+export interface Technology extends SanityBody{
+  _type:  "skill",
+  image:  Image,
+  title:  string,
+}
+
+export interface Skill extends SanityBody{
+  _type:  "skill",
+  image:  Image,
+  title:  string,
+}
+
+export interface Project extends SanityBody{
+  _type:        "project",
+  image:        Image,
+  title:        string,
+  linkToBuild:  string,
+  summary:      string,
+  technologies: Technology,
+}
+
+export interface Experience extends SanityBody {
+  _type:                  "experience",
+  company:                string,
+  companyImage:           Image,
+  dateEnded:              date,
+  dateStarted:            date,
+  isCurrentlyWorkingHere: boolean,
+  jobTitle:               string,
+  points:                 string[],
+  technologies:           Technology,
+}
+
+export interface Social extends SanityBody {
+  _type:      "social",
+  title:      string,
+  url:        string
+}
