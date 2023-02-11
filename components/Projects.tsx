7fb1a144b73f059904cefa9b4df58f40862f8372
Necralma <53120267+LihuelAlmada@users.noreply.github.com>
2023-02-11 09:08:27 -0300

    resolve errors for deploy

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Project } from "../typings";
 import { urlFor } from "../sanity";
-import project from "../sanity/schemas/project";
 
 type Props = {
   projects: Project[];
